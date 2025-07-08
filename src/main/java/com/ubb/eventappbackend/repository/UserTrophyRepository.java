edcbbe9ee9bd411150d7cfb51a1835ec6f91fe9c
Marco <63360454+marc0cl@users.noreply.github.com>
2025-07-08 01:05:10 -0400

    Add profile summary service

--- src/main/java/com/ubb/eventappbackend/repository/UserTrophyRepository.java
+++ src/main/java/com/ubb/eventappbackend/repository/UserTrophyRepository.java
@@ -5,4 +5,11 @@ import com.ubb.eventappbackend.model.UserTrophyId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserTrophyRepository extends JpaRepository<UserTrophy, UserTrophyId> {
+    /**
+     * Retrieves trophies obtained by the given user.
+     *
+     * @param userId id of the user
+     * @return list of user trophies
+     */
+    java.util.List<UserTrophy> findByUser_Id(String userId);
 }
--- src/main/java/com/ubb/eventappbackend/service/UserService.java
+++ src/main/java/com/ubb/eventappbackend/service/UserService.java
@@ -1,6 +1,7 @@
 package com.ubb.eventappbackend.service;
 
 import com.ubb.eventappbackend.model.User;
+import com.ubb.eventappbackend.model.ProfileSummary;
 
 import java.util.Optional;
 
@@ -8,4 +9,12 @@ public interface UserService {
     User registerUser(User user);
     User updateProfile(User user);
     Optional<User> findById(String id);
+
+    /**
+     * Aggregates some statistics for the user's profile page.
+     *
+     * @param userId id of the user to summarize
+     * @return summary object with counts and trophies
+     */
+    ProfileSummary getProfileSummary(String userId);
 }
